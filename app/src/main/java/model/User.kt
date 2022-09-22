c7cb10d9feeb850cd1641d281f1fc7cb358e9863
OpacareAngelus <42981839+OpacareAngelus@users.noreply.github.com>
2022-09-22 23:53:15 +0300

    ву
    ву

--- app/src/main/java/model/User.kt
+++ app/src/main/java/model/User.kt
@@ -1,7 +1,6 @@
 package com.example.level2.model
 
 data class User(
-    var id: Long,
     val photo: String,
     val name: String,
     val career: String,
---
+++ app/src/main/java/model/UsersViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.level2.model
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class UsersViewModel : ViewModel() {
+
+    private var userList : MutableLiveData<List<User>> = MutableLiveData()
+
+    init {
+        userList.value = UserData.getUsers()
+    }
+}
