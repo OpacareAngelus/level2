df850b82410d85a67d1d38fa7560a107d0b7e150
OpacareAngelus <42981839+OpacareAngelus@users.noreply.github.com>
2022-09-22 23:24:15 +0300

    de mentor
    de mentor

---
+++ app/src/main/java/data/UserData.kt
@@ -0,0 +1,36 @@
+package com.example.level2.model
+
+object UserData {
+
+    fun getUsers() = listOf(
+        User(
+            0,
+            "https://www.meme-arsenal.com/memes/2077f9271f077a9065e718d4ad7d1636.jpg",
+            "Рижик",
+            "Важлива шишка-кішка",
+            "м.Київ"
+        ),
+        User(
+            1,
+            "https://st.depositphotos.com/1072614/4661/i/600/depositphotos_46614201-stock-photo-housewife-cat.jpg",
+            "Ельвіра",
+            "Housewife",
+            "м.Львів"
+        ),
+        User(
+            2,
+            "https://funart.pro/uploads/posts/2021-07/1625629841_1-funart-pro-p-kot-uchenii-zhivotnie-krasivo-foto-2.jpg",
+            "Мірінда",
+            "Доктор котячих наук",
+            "Дніпро"
+        ),
+        User(
+            3,
+            "https://i.pinimg.com/originals/19/a8/9e/19a89e54cec871daee349e547e77056d.jpg",
+            "Андрій",
+            "Військовий",
+            "Харків"
+        )
+    )
+
+}
