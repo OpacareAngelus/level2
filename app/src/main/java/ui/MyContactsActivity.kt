a6231b3f3511475aca471b62c915e2bf2b4357e7
OpacareAngelus <42981839+OpacareAngelus@users.noreply.github.com>
2022-10-17 17:21:53 +0300

    Decomposition of main activity.
    reate clas for simpleCallback

--- app/src/main/java/ui/MyContactsActivity.kt
+++ app/src/main/java/ui/MyContactsActivity.kt
@@ -6,9 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.level2.databinding.MyContactsBinding
-import com.google.android.material.snackbar.Snackbar
 import model.User
 import model.UsersViewModel
+import util.MySimpleCallBack
 
 class MyContactsActivity : AppCompatActivity() {
 
@@ -33,7 +33,7 @@ class MyContactsActivity : AppCompatActivity() {
         val recyclerView: RecyclerView = binding.rvContacts
         recyclerView.adapter = usersAdapter
 
-        ItemTouchHelper(simpleCallback).attachToRecyclerView(recyclerView)
+        ItemTouchHelper(MySimpleCallBack(viewmodel, usersAdapter).simpleCallback).attachToRecyclerView(recyclerView)
 
         binding.tvAddContact.setOnClickListener {
             DialogFragmentAddContact().apply {
@@ -56,50 +56,5 @@ class MyContactsActivity : AppCompatActivity() {
     fun onContactSave(user: User) {
         viewmodel.userListLiveData.value?.add(user)
         usersAdapter.notifyItemRangeChanged(0, usersAdapter.itemCount)
-    }
-
-    private var simpleCallback =
-        object : ItemTouchHelper.SimpleCallback(
-            0, ItemTouchHelper.LEFT
-        ) {
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                TODO("Not yet implemented")
-            }
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                when (direction) {
-                    ItemTouchHelper.LEFT -> {
-                        val user = viewmodel.getUser(viewHolder.adapterPosition)
-                        val delMessage = Snackbar.make(
-                            viewHolder.itemView,
-                            "${user?.name} has deleted.",
-                            Snackbar.LENGTH_LONG
-                        )
-                        viewmodel.userListLiveData.value?.removeAt(viewHolder.adapterPosition)
-                        usersAdapter.notifyItemRemoved(viewHolder.adapterPosition)
-                        usersAdapter.notifyItemRangeChanged(
-                            viewHolder.adapterPosition,
-                            usersAdapter.itemCount
-                        )
-                        undoUserDeletion(user, delMessage, viewHolder.adapterPosition)
-                        delMessage.show()
-                    }
-                }
-            }
-        }
-
-    /**Method back to list of contacts deleted contact if user push "Cancel" on the Snackbar.*/
-    private fun undoUserDeletion(user: User?, delMessage: Snackbar, position: Int) {
-        delMessage.setAction("Cancel") {
-            user?.let { it1 -> viewmodel.userListLiveData.value?.add(it1) }
-            usersAdapter.notifyItemRangeChanged(
-                position,
-                usersAdapter.itemCount
-            )
-        }
     }
 }
---
+++ app/src/main/java/util/MySimpleCallBack.kt
@@ -0,0 +1,57 @@
+package util
+
+import adapter.RecyclerAdapter
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import model.User
+import model.UsersViewModel
+
+/**This is my callBack for ItemTouchHelper*/
+class MySimpleCallBack(var viewModel: UsersViewModel, var userAdapter: RecyclerAdapter) {
+
+    var simpleCallback =
+        object : ItemTouchHelper.SimpleCallback(
+            0, ItemTouchHelper.LEFT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                when (direction) {
+                    ItemTouchHelper.LEFT -> {
+                        val user = viewModel.getUser(viewHolder.adapterPosition)
+                        val delMessage = Snackbar.make(
+                            viewHolder.itemView,
+                            "${user?.name} has deleted.",
+                            Snackbar.LENGTH_LONG
+                        )
+                        viewModel.userListLiveData.value?.removeAt(viewHolder.adapterPosition)
+                        userAdapter.notifyItemRemoved(viewHolder.adapterPosition)
+                        userAdapter.notifyItemRangeChanged(
+                            viewHolder.adapterPosition,
+                            userAdapter.itemCount
+                        )
+                        undoUserDeletion(user, delMessage, viewHolder.adapterPosition)
+                        delMessage.show()
+                    }
+                }
+            }
+        }
+
+    /**Method back to list of contacts deleted contact if user push "Cancel" on the Snackbar.*/
+    private fun undoUserDeletion(user: User?, delMessage: Snackbar, position: Int) {
+        delMessage.setAction("Cancel") {
+            user?.let { it1 -> viewModel.userListLiveData.value?.add(it1) }
+            userAdapter.notifyItemRangeChanged(
+                position,
+                userAdapter.itemCount
+            )
+        }
+    }
+}
