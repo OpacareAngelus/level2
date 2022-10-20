64fcafe172b6c3ff847fca7de6dfbe2b51504de3
OpacareAngelus <42981839+OpacareAngelus@users.noreply.github.com>
2022-10-21 01:42:07 +0300

    Update RecyclerAdapterUserContacts.kt

--- app/src/main/java/adapter/RecyclerAdapterUserContacts.kt
+++ app/src/main/java/adapter/RecyclerAdapterUserContacts.kt
@@ -3,7 +3,6 @@ package adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.level2.R
@@ -11,68 +10,52 @@ import com.example.level2.databinding.RecyclerviewItemBinding
 import com.google.android.material.snackbar.Snackbar
 import extension.addImage
 import model.User
-import model.UsersViewModel
+import util.DiffUtil
 
+interface UserListController {
+    fun onDeleteUser(user: User)
+    fun onContactAdd(user: User)
+}
 
-class RecyclerAdapterUserContacts(
-    private val userList: UsersViewModel,
-    private val onDeleteUser: (User) -> Unit
-) :
-    ListAdapter<User, RecyclerAdapterUserContacts.MyViewHolder>(object : DiffUtil.ItemCallback<User>() {
+class RecyclerAdapterUserContacts(private val userListController: UserListController) :
+    ListAdapter<User, RecyclerAdapterUserContacts.ViewHolder>(DiffUtil) {
 
-        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
-            return oldItem.id == newItem.id
-        }
-
-        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
-            return oldItem == newItem
-        }
-    }),
-    View.OnClickListener {
-
-    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        lateinit var binding: RecyclerviewItemBinding
-
-        fun bind() {
-            binding = RecyclerviewItemBinding.bind(itemView)
-            binding.tvName.text = userList.getUser(adapterPosition)?.name
-            binding.tvCareer.text = userList.getUser(adapterPosition)?.career
-            userList.getUser(adapterPosition)?.let { binding.ivUserPhoto.addImage(it) }
-            binding.imgBtnTrashCan.tag = userList.getUser(adapterPosition)
-            userList.getUser(adapterPosition)?.id = adapterPosition
-
-            binding.imgBtnTrashCan.setOnClickListener {
-                deleteUser(it?.tag as User, itemView)
-            }
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.recyclerview_item, parent, false)
-        return MyViewHolder(itemView)
+        return ViewHolder(itemView)
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind()
     }
 
-    override fun onClick(view: View?) {}
-
     private fun deleteUser(user: User, view: View) {
         val delMessage = Snackbar.make(view, "${user.name} has deleted.", Snackbar.LENGTH_LONG)
-        onDeleteUser(user)
-        notifyItemRemoved(user.id)
-        userList.size()?.let { notifyItemRangeChanged(user.id, it) }
+        userListController.onDeleteUser(user)
         undoUserDeletion(user, delMessage)
         delMessage.show()
     }
 
     /**Method back to list of contacts deleted contact if user push "Cancel" on the Snackbar.*/
     private fun undoUserDeletion(user: User, delMessage: Snackbar) {
-        delMessage.setAction("Cancel") {
-            userList.add(user)
-            userList.size()?.let { it1 -> notifyItemRangeChanged(user.id, it1) }
+        delMessage.setAction(R.string.cancel) {
+            userListController.onContactAdd(user)
+        }
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind() {
+            RecyclerviewItemBinding.bind(itemView).run {
+                tvName.text = currentList[adapterPosition]?.name
+                tvCareer.text = currentList[adapterPosition]?.career
+                currentList[adapterPosition]?.let { ivUserPhoto.addImage(it) }
+                currentList[adapterPosition]?.id = adapterPosition
+
+                imgBtnTrashCan.setOnClickListener {
+                    deleteUser(currentList[adapterPosition]!!, itemView)
+                }
+            }
         }
     }
 }
